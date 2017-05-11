e7718e4028a3a1f4d22e273e06f4a49bfdd88fb5
Tony CABAYE <tony.cabaye@smile.fr>
2017-05-11 09:32:35 +0200

    Enables the Redux DevTools Extension.

--- src/src/stores/appStore.js
+++ src/src/stores/appStore.js
@@ -9,4 +9,7 @@ const reducers = combineReducers({
   navMobile
 });
 
-export default createStore(reducers);
+export default createStore(
+  reducers,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() // Enables the Redux DevTools Extension.
+);
