0d21f9e1d2dfc75fbf849eab55a5be2a32556e10
Tony CABAYE <tony.cabaye@smile.fr>
2017-06-06 14:28:34 +0200

    Correction when cookie does not exist.

--- src/src/actions/favorite.js
+++ src/src/actions/favorite.js
@@ -2,5 +2,8 @@ import Cookie from 'js-cookie';
 
 export function getItemCount() {
   const cookieName = 'cdc_wishlist_items_id';
-  return Cookie.get(cookieName).split(',').length;
+  const cookieData = Cookie.get(cookieName);
+  return cookieData
+    ? cookieData.split(',').length
+    : 0;
 }
