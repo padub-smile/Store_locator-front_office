e69fb0b7473fbbe1e8e90be0ec94134cd10adc58
Tony CABAYE <tony.cabaye@smile.fr>
2017-06-06 15:13:39 +0200

    The number of favorites and cart items is the sum of 2 cookies

--- src/src/actions/cart.js
+++ src/src/actions/cart.js
@@ -1,6 +1,10 @@
 import Cookie from 'js-cookie';
 
+const cookieNames = ['cdc_cart_items_count', 'CartItemsCount'];
+
 export function getItemCount() {
-  const cookieName = 'cdc_cart_items_count';
-  return +Cookie.get(cookieName);
+  return cookieNames
+    .map(name => Cookie.get(name))
+    .map(data => +data)
+    .reduce((a, b) => a + b, 0);
 }
--- src/src/actions/favorite.js
+++ src/src/actions/favorite.js
@@ -1,9 +1,10 @@
 import Cookie from 'js-cookie';
 
+const cookieNames = ['cdc_wishlist_items_id', 'pcd_wishlist_items_id'];
+
 export function getItemCount() {
-  const cookieName = 'cdc_wishlist_items_id';
-  const cookieData = Cookie.get(cookieName);
-  return cookieData
-    ? cookieData.split(',').length
-    : 0;
+  return cookieNames
+    .map(name => Cookie.get(name))
+    .map(data => data ? data.split(',').length : 0)
+    .reduce((a, b) => a + b, 0);
 }
