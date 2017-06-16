47dee173ce2813b82ecf91a1a712c2c805174317
Tony CABAYE <tony.cabaye@smile.fr>
2017-06-16 16:06:02 +0200

    Add default local translations.

--- src/src/app/appTranslator.js
+++ src/src/app/appTranslator.js
@@ -2,14 +2,17 @@ import { createTranslator, negociateLocale } from '@gandi/react-translate';
 import url from 'url';
 import { fetchTranslations } from '../services/locale';
 
-const defaultLocale = 'en';
+import { DEFAULT_LOCALE } from '../settings/locale';
+const translations = require(`../../public/assets/locales/${DEFAULT_LOCALE}/translations.json`);
+
 const availableLocales = ['fr', 'en'];
 const urlLocale = url.parse(document.location.href, true).path.substr(1, 2);
 const userBrowserLocales = window.navigator.languages || [(window.navigator.language || window.navigator.userLanguage)];
-const locale = negociateLocale([urlLocale, ...userBrowserLocales], availableLocales, defaultLocale);
+const locale = negociateLocale([urlLocale, ...userBrowserLocales], availableLocales, DEFAULT_LOCALE);
 const translatorParams = {
+  translations,
   locale,
-  defaultLocale
+  DEFAULT_LOCALE
 };
 const translator = createTranslator(translatorParams);
 
--- src/src/settings/locale.js
+++ src/src/settings/locale.js
@@ -1,3 +1,5 @@
+export const DEFAULT_LOCALE = 'en';
+
 export const LOCALE_TRANSLATIONS_URLS = {
   development: locale => `/assets/locales/${locale}/translations.json`,
   test: locale => `/assets/locales/${locale}/translations.json`,
